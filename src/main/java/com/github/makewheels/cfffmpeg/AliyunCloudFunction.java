5f999373d1a9fe745de38c1d79d360f83b7665b1
makewheels <makewheels@github.com>
2022-04-14 07:46:54 +0800

    init

---
+++ src/main/java/com/github/makewheels/cfffmpeg/AliyunCloudFunction.java
@@ -0,0 +1,31 @@
+package com.github.makewheels.cfffmpeg;
+
+import com.aliyun.fc.runtime.Context;
+import com.aliyun.fc.runtime.HttpRequestHandler;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+
+public class AliyunCloudFunction implements HttpRequestHandler {
+
+    @Override
+    public void handleRequest(HttpServletRequest request, HttpServletResponse response, Context context)
+            throws IOException {
+        context.getLogger().info("我是cf-ffmpeg-transcode");
+        String requestPath = (String) request.getAttribute("FC_REQUEST_PATH");
+        String requestURI = (String) request.getAttribute("FC_REQUEST_URI");
+        String requestClientIP = (String) request.getAttribute("FC_REQUEST_CLIENT_IP");
+
+        response.setStatus(200);
+        response.setHeader("header1", "value1");
+        response.setHeader("header2", "value2");
+
+        String body = String.format("Path: %s\n Uri: %s\n IP: %s\n", requestPath, requestURI, requestClientIP);
+        OutputStream out = response.getOutputStream();
+        out.write((body).getBytes());
+        out.flush();
+        out.close();
+    }
+}
