a39b444725c396b44e8965c1d47cc7e54e1d6f53
makewheels <makewheels@github.com>
2022-04-14 12:37:54 +0800

    下载文件

--- src/main/java/com/github/makewheels/cfffmpeg/AliyunCloudFunction.java
+++ src/main/java/com/github/makewheels/cfffmpeg/AliyunCloudFunction.java
@@ -1,18 +1,13 @@
 package com.github.makewheels.cfffmpeg;
 
-import cn.hutool.core.io.IoUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.aliyun.fc.runtime.Context;
 import com.aliyun.fc.runtime.HttpRequestHandler;
-import org.apache.commons.io.IOUtils;
 
-import javax.servlet.ServletInputStream;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.nio.charset.Charset;
 
 public class AliyunCloudFunction implements HttpRequestHandler {
     private TranscodeHandler transcodeHandler = new TranscodeHandler();
@@ -20,20 +15,6 @@ public class AliyunCloudFunction implements HttpRequestHandler {
     @Override
     public void handleRequest(HttpServletRequest request, HttpServletResponse response, Context context)
             throws IOException {
-        transcodeHandler.start();
-        String body = IoUtil.readUtf8(request.getInputStream());
-        System.out.println(body);
-
-        String requestPath = (String) request.getAttribute("FC_REQUEST_PATH");
-        String requestURI = (String) request.getAttribute("FC_REQUEST_URI");
-        String requestClientIP = (String) request.getAttribute("FC_REQUEST_CLIENT_IP");
-
-        response.setStatus(200);
-
-        OutputStream out = response.getOutputStream();
-        out.write((String.format("Path: %s\n Uri: %s\n IP: %s\n", requestPath,
-                requestURI, requestClientIP)).getBytes());
-        out.flush();
-        out.close();
+        transcodeHandler.start("aliyun", request, response, context);
     }
 }
--- src/main/java/com/github/makewheels/cfffmpeg/TranscodeHandler.java
+++ src/main/java/com/github/makewheels/cfffmpeg/TranscodeHandler.java
@@ -1,13 +1,74 @@
 package com.github.makewheels.cfffmpeg;
 
 import cn.hutool.core.io.FileUtil;
+import cn.hutool.core.io.IoUtil;
+import cn.hutool.core.io.file.FileNameUtil;
 import cn.hutool.http.HttpUtil;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
+import com.aliyun.fc.runtime.Context;
+import com.aliyun.fc.runtime.Credentials;
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.model.GetObjectRequest;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.File;
+import java.io.IOException;
 
 public class TranscodeHandler {
     private File workDir = new File(System.getenv("work_dir"));
     private File ffmpegFile;
+    private File transcodeFolder;
+    private File inputFile;
+    private File outputFolder;
+    private File m3u8File;
+
+    private String provider;
+    private HttpServletRequest request;
+    private HttpServletResponse response;
+    private Object contextObject;
+    private JSONObject body;
+    private String bucket;
+    private String endpoint;
+    private String inputKey;
+    private String m3u8Key;
+    private String accessKeyId;
+    private String accessKeySecret;
+    private String sessionToken;
+
+    private OSS ossClient;
+
+    private String videoId;
+
+    /**
+     * 准备ffmpeg
+     */
+    private void prepareArgs(String provider, HttpServletRequest request, HttpServletResponse response,
+                             Object contextObject) throws IOException {
+        this.provider = provider;
+        this.request = request;
+        this.response = response;
+        this.contextObject = contextObject;
+
+        Context context = (Context) contextObject;
+        Credentials credentials = context.getExecutionCredentials();
+        bucket = body.getString("bucket");
+        endpoint = body.getString("endpoint");
+        inputKey = body.getString("inputKey");
+        m3u8Key = body.getString("m3u8Key");
+        accessKeyId = credentials.getAccessKeyId();
+        accessKeySecret = credentials.getAccessKeySecret();
+        sessionToken = credentials.getSecurityToken();
+
+        body = JSON.parseObject(IoUtil.readUtf8(request.getInputStream()));
+        videoId = body.getString("videoId");
+        transcodeFolder = new File(workDir, videoId);
+        inputFile = new File(transcodeFolder, FileNameUtil.mainName(inputKey));
+        outputFolder = new File(transcodeFolder, "out");
+        m3u8File = new File(outputFolder, "index.m3u8");
+    }
 
     /**
      * 准备ffmpeg
@@ -25,8 +86,31 @@ public class TranscodeHandler {
         }
     }
 
-    public void start() {
+    /**
+     * 下载原始文件
+     */
+    private void prepareInputFile() {
+        if (provider.equals("aliyun")) {
+            ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret, sessionToken);
+            ossClient.getObject(new GetObjectRequest(bucket, inputKey), inputFile);
+        }
+    }
+
+    /**
+     * 从这里开始
+     *
+     * @param provider
+     * @param request
+     * @param response
+     * @param contextObject
+     * @throws IOException
+     */
+    public void start(String provider, HttpServletRequest request, HttpServletResponse response,
+                      Object contextObject) throws IOException {
+        prepareArgs(provider, request, response, contextObject);
         prepareFFmpeg();
+        prepareInputFile();
 
     }
+
 }
