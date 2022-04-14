2e045a505b09f06ea0f48037a7f1a4c0aa0c2425
makewheels <makewheels@github.com>
2022-04-14 08:36:43 +0800

    test

--- src/main/java/com/github/makewheels/cfffmpeg/TranscodeHandler.java
+++ src/main/java/com/github/makewheels/cfffmpeg/TranscodeHandler.java
@@ -1,5 +1,6 @@
 package com.github.makewheels.cfffmpeg;
 
+import cn.hutool.core.io.FileUtil;
 import cn.hutool.http.HttpUtil;
 
 import java.io.File;
@@ -18,6 +19,7 @@ public class TranscodeHandler {
         if (!ffmpegFile.exists()) {
             String ffmpegUrl = "https://common-objects.oss-cn-beijing.aliyuncs.com" +
                     "/ffmpeg/linux-static-builds/amd64/5.0.1/ffmpeg-5.0.1-amd64-static/ffmpeg";
+            FileUtil.mkdir(ffmpegFolder);
             HttpUtil.downloadFile(ffmpegUrl, ffmpegFile);
         }
     }
