13099e00c44432cebdb6118138f9f8c55c5fca4a
makewheels <makewheels@github.com>
2022-04-21 15:47:53 +0800

    准备镜像，测试GPU

--- src/main/java/com/github/makewheels/cfffmpeg/functions/gpu/GpuController.java
+++ src/main/java/com/github/makewheels/cfffmpeg/functions/gpu/GpuController.java
@@ -1,12 +1,12 @@
 package com.github.makewheels.cfffmpeg.functions.gpu;
 
 import cn.hutool.core.util.IdUtil;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class GpuController {
-    @PostMapping("/")
+    @RequestMapping("/")
     public String hello() {
         return "this is gpu controller" + IdUtil.simpleUUID();
     }
